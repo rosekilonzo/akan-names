26013eefeb0b275cd8e6e77ceab7c26597c1dbed
rose kilonzo <kdianarose903@gmail.com>
2021-06-13 19:27:07 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 function naming (){
     var CC=parseInt(document.getElementById('main').value);
-    var YY=parseInt()
+    var YY=parseInt(document.getElementById('name').value);
+    var MM=parseInt(document.getElementById('form').value);
+    var DD=parseInt(document.getElementById('akan').value);
+
+    var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var femaleNames=['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Ama'];
+    var maleNames=['Kwasi','Kwadwo','Kwabwena','Kwaku','Yaw','Kofi','Kwame';
+
+if (CC.value==''||YY.value==''MM.value==''||DD.value==''){
+    alert('No blank values allowed');
+}
+
 }
